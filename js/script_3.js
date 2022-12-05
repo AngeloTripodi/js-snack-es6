5e2f8f81f9d1a5a17c91cf200125e908baf1a027
AngeloTripodi <tripodi.angelo@gmail.com>
2022-12-05 12:40:38 +0100

    Add avg Weight and Lenght

--- js/script_3.js
+++ js/script_3.js
@@ -1,42 +1,42 @@
 const zucchini = [
 {
     type : "Italiana",
-    weight : 3,
+    weight : 120,
     length : 0.22
 },
 {
     type : "Brasiliana",
-    weight : 1,
+    weight : 130,
     length : 0.30
 },
 {
     type : "Russa",
-    weight : 4,
+    weight : 100,
     length : 0.88
 },
 {
     type : "Cinese",
-    weight : 1,
+    weight : 80,
     length : 0.02
 },
 {
     type : "Congolese",
-    weight : 6,
+    weight : 100,
     length : 1.22
 },
 {
     type : "Vietnamita",
-    weight : 2,
+    weight : 100,
     length : 1.42
 },
 {
     type : "Belga",
-    weight : 2,
+    weight : 120,
     length : 8.22
 },
 {
     type : "Spagnola",
-    weight : 22,
+    weight : 110,
     length : 9.22
 },
 {
@@ -46,13 +46,22 @@ const zucchini = [
 },
 {
     type : "Canadese",
-    weight : 1,
+    weight : 100,
     length : 1
 },
 ]
+
 sum = 0;
+avgWeight = 0;
+avgLenght = 0;
 for (let i = 0; i<zucchini.length; i++){
     sum += zucchini[i].weight;
+    avgWeight += zucchini[i].weight / 10;
+    avgLenght += zucchini[i].length / 10;
+
+
 }
 
-console.log(sum)
+console.log("Tutte le zucchine pesano" + " " + sum + "Kg");
+console.log("Il peso medio è" + " " + avgWeight + "Kg");
+console.log("La lunghezza media è " + " " + avgLenght + "cm");
