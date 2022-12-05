c3c44107fda9d40dbc8300944411bdca235b1558
AngeloTripodi <tripodi.angelo@gmail.com>
2022-12-05 12:29:41 +0100

    setup snacks 3

---
+++ js/script_3.js
@@ -0,0 +1,55 @@
+const zucchini = [
+{
+    type : "Italiana",
+    weight : 3,
+    length : 0.22
+},
+{
+    type : "Brasiliana",
+    weight : 1,
+    length : 0.30
+},
+{
+    type : "Russa",
+    weight : 4,
+    length : 0.88
+},
+{
+    type : "Cinese",
+    weight : 1,
+    length : 0.02
+},
+{
+    type : "Congolese",
+    weight : 6,
+    length : 1.22
+},
+{
+    type : "Vietnamita",
+    weight : 2,
+    length : 1.42
+},
+{
+    type : "Belga",
+    weight : 2,
+    length : 8.22
+},
+{
+    type : "Spagnola",
+    weight : 22,
+    length : 9.22
+},
+{
+    type : "Taiwanese",
+    weight : 90,
+    length : 122
+},
+{
+    type : "Canadese",
+    weight : 1,
+    length : 1
+},
+
+
+
+]
