14f099fa22444fdda0c94e2f64a2ba6574fc217c
AngeloTripodi <tripodi.angelo@gmail.com>
2022-12-05 11:44:04 +0100

    Add array

--- js/script.js
+++ js/script.js
@@ -4,8 +4,40 @@ const students = {
     age : 28
 }
 
-const studentsList = 
+const studentsList = [ 
+    {
+        name : "Luigi",
+        lastname : "Luigioso",
+        age : 30
+    },
+    {
+        name : "Mario",
+        lastname : "Marioso",
+        age : 35
+    },
+    {
+        name : "Virginia",
+        lastname : "Virginiosa",
+        age : 20
+    },
+    {
+        name : "Romolo",
+        lastname : "Romoloso",
+        age : 1500
+    },
+    {
+        name : "Cesare",
+        lastname : "Cesaroso",
+        age : 18
+    },
 
-for (let key in students) {
-    console.log(key ,students[key]);
+]
+
+for (let i = 0; i < studentsList.length; i++ ){
+    for (let key in students) {
+        // console.log(key ,students[key]);
+        console.log(studentsList[i][key])
+    
+    }
 }
+
