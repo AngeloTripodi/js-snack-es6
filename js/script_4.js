bfa1a64fb63b75f27608c0d056e7579e5542b935
AngeloTripodi <tripodi.angelo@gmail.com>
2022-12-06 11:56:30 +0100

    setup snack 5

--- js/script_4.js
+++ js/script_4.js
@@ -22,7 +22,7 @@ const listaMammiferi = animali.filter((tipo) => {
 const nameToUppercase = animali.map((name) => {
     return name.nome.charAt(0) + name.nome.substring(1).toUpperCase()
 });
-
-console.log(nameToUppercase)
-
 console.log(listaMammiferi);
+console.log(nameToUppercase);
+
+
---
+++ js/script_5.js
@@ -0,0 +1,13 @@
+const persone = [
+    { nome: 'angelo', cognome: 'tripodi', eta: 28 },
+    { nome: 'angela', cognome: 'tripodi', eta: 90 },
+    { nome: 'wanna', cognome: 'marchi', eta: 9 },
+    { nome: 'ciccio', cognome: 'de ciccis', eta: 32 },
+    { nome: 'roberto', cognome: 'de robertis', eta: 8 },
+    { nome: 'riccardo', cognome: 'de riccardis', eta: 11 },
+    { nome: 'antonio', cognome: 'antoni', eta: 42 },
+    { nome: 'milena', cognome: 'de milenis', eta: 12 },
+    { nome: 'sofia', cognome: 'sofi', eta: 2 },
+    { nome: 'marco', cognome: 'marchi', eta: 17 }
+]
+
